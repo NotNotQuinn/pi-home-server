bdb3ae0a24a7da780c58bf8b10fec0a10bfd9bcc
Quinn T <Quinn.GitHub@gmail.com>
2022-04-05 20:53:28 -0500

    Print publishes

--- websub/main.go
+++ websub/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/notnotquinn/go-websub"
 	"github.com/notnotquinn/pi-home-server/config"
 )
+
+var log = websub.Logger()
 
 func main() {
 	c, err := config.Load("./config.yaml")
@@ -14,14 +17,22 @@ func main() {
 		panic(err)
 	}
 
-	http.ListenAndServe(fmt.Sprintf(":%d", c.Hub.Port),
-		websub.NewHub(c.Hub.URL,
-			websub.HubAllowPostBodyAsContent(true),
-			websub.HubExposeTopics(true),
-			websub.HubWithUserAgent("pi-home-server-hub"),
-			websub.HubWithHashFunction("sha512"),
-		),
+	h := websub.NewHub(c.Hub.URL,
+		websub.HubAllowPostBodyAsContent(true),
+		websub.HubExposeTopics(true),
+		websub.HubWithUserAgent("pi-home-server-hub"),
+		websub.HubWithHashFunction("sha512"),
 	)
+
+	h.AddSniffer("", func(topic, contentType string, body io.Reader) {
+		content, err := io.ReadAll(body)
+		if err != nil {
+			log.Err(err).Msg("could not read publish body")
+		}
+		fmt.Printf("Publish: %s (%s)\n  %s\n", topic, contentType, string(content))
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%d", c.Hub.Port), h)
 
 	<-make(chan struct{})
 }
